eac519cef864625f992c0020458c22e2f9d73391
jrodparker-dev <jrodparker@gmail.com>
2025-09-23 15:10:51 -0500

    all terrain image

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (0edda540/b472a8ce)";
+Config.version = "0.11.2 (000b0841/b472a8ce)";
 
 Config.routes = {
 	root: '191.101.232.116',
