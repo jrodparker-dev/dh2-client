000b08416df1ed02685363c98714c213aa520ab0
jrodparker-dev <jrodparker@gmail.com>
2025-09-23 13:16:33 -0500

    Gasoline png working

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (1f2ce133/b472a8ce)";
+Config.version = "0.11.2 (0edda540/b472a8ce)";
 
 Config.routes = {
 	root: '191.101.232.116',
