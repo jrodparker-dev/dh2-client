30249ecff329e413b95c23ccaff85766bbd93049
jrodparker-dev <jrodparker@gmail.com>
2025-10-02 15:49:14 -0500

    dunno if anything changed, just pushing it

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (f18f8579/b472a8ce)";
+Config.version = "0.11.2 (00c2a3f8/b472a8ce)";
 
 Config.routes = {
 	root: '191.101.232.116',
