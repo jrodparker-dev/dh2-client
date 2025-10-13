fa58b0a8ef47119fee6f6113611e96fcfd07dba4
jrodparker-dev <jrodparker@gmail.com>
2025-10-13 11:05:33 -0500

    changed position of baked in typechart

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (abe03890/b472a8ce)";
+Config.version = "0.11.2 (d303e1a8/b472a8ce)";
 
 Config.routes = {
 	root: '191.101.232.116',
