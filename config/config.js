e8cd53a0ca46f23a1368ab833c1b541f2c944306
jrodparker-dev <jrodparker@gmail.com>
2025-10-20 17:35:39 -0500

    change avatar list popup to use my sheet

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (2b99d78f/b472a8ce)";
+Config.version = "0.11.2 (d61216ff/b472a8ce)";
 
 Config.routes = {
 	root: '191.101.232.116',
